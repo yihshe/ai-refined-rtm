46accf746b105fff557486c51e204f856b1f8731
yihshe <61969076+yihshe@users.noreply.github.com>
2023-05-10 09:19:09 +0000

    histogram of trainset

---
+++ visuals/histogram.py
@@ -0,0 +1,42 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+CSV_PATH = '/maps/ys611/ai-refined-rtm/data/BPWW_extract_2018_reshaped_train.csv'
+SAVE_PATH = '/maps/ys611/ai-refined-rtm/visuals/histograms'
+S2_BANDS = ['B02_BLUE', 'B03_GREEN', 'B04_RED', 'B05_RE1', 'B06_RE2',
+            'B07_RE3', 'B08_NIR1', 'B8A_NIR2', 'B09_WV', 'B11_SWI1',
+            'B12_SWI2']
+NUM_BINS = 50
+# mkdir if the save path does not exist
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+# read the csv file
+df = pd.read_csv(CSV_PATH)
+# plot histogram for each band using seaborn
+# %%
+# create individual figures for each band
+for band in S2_BANDS:
+    sns.histplot(df[band].values, bins=NUM_BINS)
+    plt.title(f'Training Data: Histogram of {band}')
+    plt.xlabel('Reflectance')
+    plt.ylabel('Frequency')
+    plt.tight_layout()
+    plt.savefig(os.path.join(SAVE_PATH, f'histogram_trainset_{band}.png'))
+    plt.show()
+
+# %%
+# create one figure and plot each band as a subplot
+fig, axs = plt.subplots(3, 4, figsize=(20, 15))
+for i, band in enumerate(S2_BANDS):
+    sns.histplot(df[band].values, bins=NUM_BINS, ax=axs[i//4, i % 4])
+    axs[i//4, i % 4].set_title(f'Histogram of {band}')
+    axs[i//4, i % 4].set_xlabel('Reflectance')
+    axs[i//4, i % 4].set_ylabel('Frequency')
+plt.tight_layout()
+plt.savefig(os.path.join(SAVE_PATH, 'histogram_trainset.png'), dpi=300)
+plt.show()
+
+# %%
