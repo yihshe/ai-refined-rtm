40b7ddd48a7f7f883e5eb4ae3d1e25c747f35b26
yihshe <61969076+yihshe@users.noreply.github.com>
2023-05-27 15:12:14 +0000

    clear up

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-python3 -m pdb temp.py
+# python3 -m pdb temp.py
 # python3 -m pdb rtm_torch/Resources/PROSAIL/prospect.py
-# python3 -m pdb rtm_unittest.py
+python3 -m pdb rtm_unittest.py
