ffc8936d2d53e69db97e4fb422bdec76d343d845
yihshe <61969076+yihshe@users.noreply.github.com>
2023-06-09 09:52:01 +0000

    3d plots

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 # python3 -m pdb test.py --config configs/vanilla_AE_scaled.json \
 #         --resume saved/models/VanillaAE_scaled/0510_211403/model_best.pth
 
+python3 -m pdb test.py --config configs/AE_RTM.json \
+        --resume /maps/ys611/ai-refined-rtm/saved/models/AE_RTM/0607_164242/model_best.pth
+
 # Generate synthetic data from RTM
 # python3 datasets/sampling/sampling.py
 
@@ -19,8 +22,7 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 # python3 -m pdb datasets/preprocessing/standardize.py
 
 # Train a simple vanilla autoencoder with RTM
-python3 -m pdb train.py --config configs/AE_RTM.json 
-        # --resume /maps/ys611/ai-refined-rtm/saved/models/NNRegressor/0605_183155/model_best.pth
+# python3 -m pdb train.py --config configs/AE_RTM.json 
 
 # Train NN Regressor
 # python3 -m pdb train.py --config configs/NN_regressor.json 
